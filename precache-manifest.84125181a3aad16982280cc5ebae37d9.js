1ce189217f22c10355eb7222174f68bdea5fc99a
rshaikhha <rshaikhha@users.noreply.github.com>
2019-05-30 02:46:45 +0430

    Updates

---
+++ precache-manifest.84125181a3aad16982280cc5ebae37d9.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "385943316bd2be6b65680f0e5c1900ba",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "96d3427e3db9f8e8dbe9",
+    "url": "/react-gh-pages/static/css/main.f8ae046f.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/react-gh-pages/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "96d3427e3db9f8e8dbe9",
+    "url": "/react-gh-pages/static/js/main.5410a315.chunk.js"
+  },
+  {
+    "revision": "a0c43bde80f322238324",
+    "url": "/react-gh-pages/static/js/runtime~main.ac2d5040.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/react-gh-pages/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/react-gh-pages/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/react-gh-pages/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/react-gh-pages/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/react-gh-pages/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-gh-pages/precache-manifest.cae92349879c43c936844ca6b9055730.js"
+  "/react-gh-pages/precache-manifest.84125181a3aad16982280cc5ebae37d9.js"
 );
 
 self.addEventListener('message', (event) => {
